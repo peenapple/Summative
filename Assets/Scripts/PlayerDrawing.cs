cb5fdb899425b421daee913a7eab24dcf389b149
peenapple <justinye787@gmail.com>
2023-12-14 09:31:38 -0500

    continuous drawing bug fix

--- Assets/Scripts/PlayerDrawing.cs
+++ Assets/Scripts/PlayerDrawing.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class PlayerDrawing : MonoBehaviour
 {
     public GameObject prefabToInstantiate;
+    Vector3 previousMousePosition;
+
+    void Start()
+    {
+        // previous mouse position
+        previousMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        previousMousePosition.z = 0f;
+    }
 
     void Update()
     {
         if (Input.GetMouseButton(0)) // check for left mouse click
         {
-            // get the mouse position in the world coordinates
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f; // set the Z position to 0 to place it in the 2D space
+            // get the current mouse position in the world coordinates
+            Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentMousePosition.z = 0f;
 
-            Instantiate(prefabToInstantiate, mousePosition, Quaternion.identity);
-            // instantiate the prefab at the mouse click position with no rotation
+            // check if the current mouse position is different from the previous position
+            if (currentMousePosition != previousMousePosition)
+            {
+                // instantiate the prefab if the mouse has moved
+                Instantiate(prefabToInstantiate, currentMousePosition, Quaternion.identity);
+                // update the previous mouse position
+                previousMousePosition = currentMousePosition;
+            }
         }
     }
 }
