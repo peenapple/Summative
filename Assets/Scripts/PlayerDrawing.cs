997aae3a31ec75a1ba287e2cfdc7de6d1860d6f7
peenapple <justinye787@gmail.com>
2023-12-14 19:25:38 -0500

    draw limit counter + polygon collider change

--- Assets/Scripts/PlayerDrawing.cs
+++ Assets/Scripts/PlayerDrawing.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerDrawing : MonoBehaviour
 {
-    public GameObject prefabToInstantiate;
+    public GameObject prefabToInstantiate; 
     Vector3 previousMousePosition;
+    public TextMeshProUGUI count;
+    private int drawCounter = 100;
 
     void Start()
     {
@@ -16,19 +19,27 @@ public class PlayerDrawing : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButton(0)) // check for left mouse click
+        // if draw counter greater than 0
+        if (drawCounter > 0)
         {
-            // get the current mouse position in the world coordinates
-            Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentMousePosition.z = 0f;
-
-            // check if the current mouse position is different from the previous position
-            if (currentMousePosition != previousMousePosition)
+            if (Input.GetMouseButton(0)) // check for left mouse click
             {
-                // instantiate the prefab if the mouse has moved
-                Instantiate(prefabToInstantiate, currentMousePosition, Quaternion.identity);
-                // update the previous mouse position
-                previousMousePosition = currentMousePosition;
+                // get the current mouse position in the world coordinates
+                Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                currentMousePosition.z = 0f;
+
+                // check if the current mouse position is different from the previous position
+                if (currentMousePosition != previousMousePosition)
+                {
+                    // instantiate the prefab if the mouse has moved
+                    Instantiate(prefabToInstantiate, currentMousePosition, Quaternion.identity);
+                    // update the previous mouse position
+                    previousMousePosition = currentMousePosition;
+
+                    // update draw counter
+                    drawCounter--;
+                    count.text = drawCounter.ToString() + "/100";
+                }
             }
         }
     }
