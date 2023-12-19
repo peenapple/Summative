d7bd5f976a88042c539befbc70c426bf26533507
peenapple <justinye787@gmail.com>
2023-12-18 22:21:47 -0500

    WORKING PAUSE MENU (except for home lol XD)

--- Assets/Scripts/PlayerPlacing.cs
+++ Assets/Scripts/PlayerPlacing.cs
@@ -10,6 +10,9 @@ public class PlayerDrawing : MonoBehaviour
     public TextMeshProUGUI count;
     private int placeCounter = 3;
 
+    [SerializeField] RectTransform pauseBtn;
+    [SerializeField] Transform pauseMenu;
+
     void Start()
     {
         // previous mouse position
@@ -19,27 +22,24 @@ public class PlayerDrawing : MonoBehaviour
 
     void Update()
     {
-        // if draw counter greater than 0
-        if (placeCounter > 0)
+        // if draw counter greater than 0, pause menu is not active and player places a platform
+        if (Input.GetMouseButtonDown(0) && placeCounter > 0 && !pauseMenu.gameObject.activeSelf)
         {
-            if (Input.GetMouseButtonDown(0)) // check for left mouse click
+            // get the current mouse position in the world coordinates
+            Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentMousePosition.z = 0f;
+
+            // check if the current mouse position is different from the previous position
+            if (currentMousePosition != previousMousePosition && !RectTransformUtility.RectangleContainsScreenPoint(pauseBtn, currentMousePosition))
             {
-                // get the current mouse position in the world coordinates
-                Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                currentMousePosition.z = 0f;
+                // instantiate the prefab if the mouse has moved
+                Instantiate(prefabToInstantiate, currentMousePosition, Quaternion.identity);
+                // update the previous mouse position
+                previousMousePosition = currentMousePosition;
 
-                // check if the current mouse position is different from the previous position
-                if (currentMousePosition != previousMousePosition)
-                {
-                    // instantiate the prefab if the mouse has moved
-                    Instantiate(prefabToInstantiate, currentMousePosition, Quaternion.identity);
-                    // update the previous mouse position
-                    previousMousePosition = currentMousePosition;
-
-                    // update draw counter
-                    placeCounter--;
-                    count.text = placeCounter.ToString() + "/3";
-                }
+                // update draw counter
+                placeCounter--;
+                count.text = placeCounter.ToString() + "/3";
             }
         }
     }
