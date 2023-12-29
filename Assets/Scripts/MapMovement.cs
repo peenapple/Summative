a074aa798b9e478d8073eb9884094fe5fcf2d56d
peenapple <justinye787@gmail.com>
2023-12-29 13:18:17 -0500

    map movement

--- Assets/Scripts/MapMovement.cs
+++ Assets/Scripts/MapMovement.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class MapMovement : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float tweenTime = 0.6f;
+    [SerializeField] float tweenTime = 1f;
     [SerializeField] LeanTweenType tweenType;
 
     void Update()
     {
-        if (player.position.x == -4.03f)
+        if (Mathf.Abs(player.position.x + 4.01f) < 0.01f)
         {
             if (Input.GetKeyDown(KeyCode.D))
             {
-                LeanTween.moveLocalX(player.gameObject, 0.95f, tweenTime).setEase(tweenType);
-                LeanTween.moveLocalX(Camera.main.transform.gameObject, 1, tweenTime).setEase(tweenType);
+                LeanTween.moveLocalX(player.gameObject, 0.965f, tweenTime).setEase(tweenType);
+                LeanTween.moveLocalX(Camera.main.transform.gameObject, 2, tweenTime).setEase(tweenType);
+            }
+        }
+        else if (Mathf.Abs(player.position.x - 0.965f) < 0.01f)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                LeanTween.moveLocalX(player.gameObject, 5.825f, tweenTime).setEase(tweenType);
+                LeanTween.moveLocalX(Camera.main.transform.gameObject, 4, tweenTime).setEase(tweenType);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                LeanTween.moveLocalX(player.gameObject, -4.01f, tweenTime).setEase(tweenType);
+                LeanTween.moveLocalX(Camera.main.transform.gameObject, 0, tweenTime).setEase(tweenType);
+            }
+        }
+        else if (Mathf.Abs(player.position.x - 5.825f) < 0.01f)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                LeanTween.moveLocalX(player.gameObject, 10.6f, tweenTime).setEase(tweenType);
+                LeanTween.moveLocalX(Camera.main.transform.gameObject, 6, tweenTime).setEase(tweenType);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                LeanTween.moveLocalX(player.gameObject, 0.965f, tweenTime).setEase(tweenType);
+                LeanTween.moveLocalX(Camera.main.transform.gameObject, 2, tweenTime).setEase(tweenType);
             }
         }
     }
