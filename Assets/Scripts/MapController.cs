7c330a7dc2095d24c5845b113da8360afb42010e
peenapple <justinye787@gmail.com>
2023-12-29 14:07:44 -0500

    PREVEIEWS

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -2,25 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapMovement : MonoBehaviour
+public class MapController : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] float tweenTime = 1f;
     [SerializeField] LeanTweenType tweenType;
 
+    [SerializeField] Transform preview1;
+    [SerializeField] Transform preview2;
+    [SerializeField] Transform preview3;
+
     void Update()
     {
-        // player + camera movement with the map
+        // player + camera movement with the map for level 1
         if (Mathf.Abs(player.position.x + 4.01f) < 0.01f)
         {
+            // previews
+            preview1.gameObject.SetActive(true);
+            preview2.gameObject.SetActive(false);
+            preview3.gameObject.SetActive(false);
+
             if (Input.GetKeyDown(KeyCode.D))
             {
                 LeanTween.moveLocalX(player.gameObject, 0.965f, tweenTime).setEase(tweenType);
                 LeanTween.moveLocalX(Camera.main.transform.gameObject, 2, tweenTime).setEase(tweenType);
             }
         }
-        else if (Mathf.Abs(player.position.x - 0.965f) < 0.01f)
+        else
         {
+            preview1.gameObject.SetActive(false);
+        }
+
+        // for level 2
+        if (Mathf.Abs(player.position.x - 0.965f) < 0.01f)
+        {
+            // previews
+            preview1.gameObject.SetActive(false);
+            preview2.gameObject.SetActive(true);
+            preview3.gameObject.SetActive(false);
+
             if (Input.GetKeyDown(KeyCode.D))
             {
                 LeanTween.moveLocalX(player.gameObject, 5.825f, tweenTime).setEase(tweenType);
@@ -32,8 +52,19 @@ public class MapMovement : MonoBehaviour
                 LeanTween.moveLocalX(Camera.main.transform.gameObject, 0, tweenTime).setEase(tweenType);
             }
         }
-        else if (Mathf.Abs(player.position.x - 5.825f) < 0.01f)
+        else
         {
+            preview2.gameObject.SetActive(false);
+        }
+
+        // for level 3
+        if (Mathf.Abs(player.position.x - 5.825f) < 0.01f)
+        {
+            // previews
+            preview1.gameObject.SetActive(false);
+            preview2.gameObject.SetActive(false);
+            preview3.gameObject.SetActive(true);
+
             if (Input.GetKeyDown(KeyCode.D))
             {
                 LeanTween.moveLocalX(player.gameObject, 10.6f, tweenTime).setEase(tweenType);
@@ -44,6 +75,10 @@ public class MapMovement : MonoBehaviour
                 LeanTween.moveLocalX(player.gameObject, 0.965f, tweenTime).setEase(tweenType);
                 LeanTween.moveLocalX(Camera.main.transform.gameObject, 2, tweenTime).setEase(tweenType);
             }
+        }
+        else
+        {
+            preview3.gameObject.SetActive(false);
         }
     }
 }
