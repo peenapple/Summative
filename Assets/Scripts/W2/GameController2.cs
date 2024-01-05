12f22771eac39b6dc335c08b2e2c7a167420a4c4
peenapple <justinye787@gmail.com>
2024-01-04 22:55:26 -0500

    game controller TWO

---
+++ Assets/Scripts/W2/GameController2.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
+public class GameController2 : MonoBehaviour
+{
+    Vector2 startPos;
+    Rigidbody2D playerRb;
+    ParticleSystem particles;
+
+    [SerializeField] Transform endScreen;
+    [SerializeField] Transform pauseMenu;
+    [SerializeField] float tweenTime = 0.3f;
+    [SerializeField] LeanTweenType tweenType;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        particles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        // spawn point
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if you collide with obstacle
+        if (collision.gameObject.CompareTag("Obstacle") || collision.gameObject.CompareTag("Saw"))
+        {
+            Die();
+        }
+        // if you collide with end flag
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            UnlockNewLevel();
+            endScreen.gameObject.SetActive(true);
+            LeanTween.moveLocalY(endScreen.gameObject, 0, tweenTime).setEase(tweenType);
+        }
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex2"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex2", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel2", PlayerPrefs.GetInt("UnlockedLevel2", 0) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
+    void Die()
+    {
+        // death effect
+        particles.Play();
+        // wait for 0.5 seconds before respawn
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        playerRb.simulated = false;
+        playerRb.velocity = new Vector2(0, 0);
+        transform.localScale = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(duration);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Pause()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("W2");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("W2");
+        Time.timeScale = 1;
+    }
+}
