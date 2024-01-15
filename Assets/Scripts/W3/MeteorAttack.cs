a2a1f5a8c72376ea9cc868842aec7428d183bd06
peenapple <justinye787@gmail.com>
2024-01-15 17:28:58 -0500

    meteor Works

--- Assets/Scripts/W3/MeteorAttack.cs
+++ Assets/Scripts/W3/MeteorAttack.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class MeteorAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float restDuration = 0f; // Duration to rest before starting
+    ParticleSystem particles;
+
+    [SerializeField] float tweenTime = 6f;
+
+    private void Awake()
     {
-        
+        particles = GetComponentInChildren<ParticleSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(Rest(restDuration));
+    }
+    IEnumerator Rest(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        LeanTween.moveLocalX(transform.gameObject, -1.7f, tweenTime);
+        LeanTween.moveLocalY(transform.gameObject, -6.13f, tweenTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LevelPlatform") || collision.gameObject.CompareTag("Platform") || collision.gameObject.CompareTag("Deactivator"))
+        {
+            // death effect
+            particles.Play();
+            transform.localScale = new Vector3(0, 0, 0);
+            StartCoroutine(Die(0.5f));
+        }
+    }
+
+    IEnumerator Die(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(transform.gameObject);
     }
 }
