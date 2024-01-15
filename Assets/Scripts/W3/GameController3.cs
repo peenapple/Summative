b5a0c96368ad72f1e711c2a8f8980c0b8845a484
peenapple <justinye787@gmail.com>
2024-01-15 11:08:27 -0500

    made some brown things

---
+++ Assets/Scripts/W3/GameController3.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
+public class GameController3 : MonoBehaviour
+{
+    Vector2 startPos;
+    Rigidbody2D playerRb;
+    ParticleSystem particles;
+
+    [SerializeField] Button pause;
+    [SerializeField] Transform endScreen;
+    [SerializeField] Transform pauseMenu;
+    [SerializeField] float tweenTime = 0.3f;
+    [SerializeField] LeanTweenType tweenType;
+
+    public int scrollSpeed;
+    public int scrollDistance;
+    private bool finish = false;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        particles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        // spawn point
+        startPos = transform.position;
+        // main camera scrolling
+        LeanTween.moveLocalX(Camera.main.transform.gameObject, scrollDistance, scrollSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if you collide with obstacle
+        if (!finish)
+        {
+            if (collision.gameObject.CompareTag("Obstacle") || collision.gameObject.CompareTag("Saw"))
+            {
+                Die();
+            }
+        }
+        // if you collide with end flag
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            UnlockNewLevel();
+            pause.interactable = false;
+            endScreen.gameObject.SetActive(true);
+            LeanTween.moveLocalY(endScreen.gameObject, 0, tweenTime).setEase(tweenType);
+            finish = true;
+        }
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex3"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex3", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel3", PlayerPrefs.GetInt("UnlockedLevel3", 0) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
+    void Die()
+    {
+        // death effect
+        particles.Play();
+        // wait for 0.5 seconds before respawn
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        playerRb.simulated = false;
+        playerRb.velocity = new Vector2(0, 0);
+        transform.localScale = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(duration);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Pause()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("W3");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("W3");
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Scripts/W3/PlayerPlacing3.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerPlacing3 : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    public GameObject deactPrefab;
+    Vector3 previousMousePosition;
+
+    public TextMeshProUGUI platCount;
+    public TextMeshProUGUI deactCount;
+
+    public int platCounter = 20;
+    public int deactCounter = 3;
+    private int initialPlatCount;
+    private int initialDeactCount;
+    private bool platActive = true;
+    private bool deactActive = false;
+
+    [SerializeField] RectTransform pauseBtn;
+    [SerializeField] Transform pauseMenu;
+    [SerializeField] Transform endScreen;
+    [SerializeField] Transform border1;
+    [SerializeField] Transform border2;
+
+    void Start()
+    {
+        // previous mouse position
+        previousMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        previousMousePosition.z = 0f;
+        platActive = true;
+
+        initialPlatCount = platCounter;
+        initialDeactCount = deactCounter;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            platActive = true;
+            deactActive = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            deactActive = true;
+            platActive = false;
+        }
+
+        // Check for right mouse button click
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Raycast to get the object under the mouse in 2D
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                // Check if the object has the "platform" tag
+                if (hit.collider.CompareTag("Platform"))
+                {
+                    // Destroy the object
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+
+        // for platforms
+        if (platActive)
+        {
+            border1.gameObject.SetActive(true);
+            border2.gameObject.SetActive(false);
+
+            // if draw counter greater than 0, pause menu is not active and player places a platform
+            if (Input.GetMouseButtonDown(0) && platCounter > 0 && !pauseMenu.gameObject.activeSelf && !endScreen.gameObject.activeSelf)
+            {
+                // get the current mouse position in the world coordinates
+                Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                currentMousePosition.z = 0f;
+
+                // check if the current mouse position is different from the previous position, and if it's not on the pause button
+                if (currentMousePosition != previousMousePosition && !RectTransformUtility.RectangleContainsScreenPoint(pauseBtn, currentMousePosition))
+                {
+                    // instantiate the prefab if the mouse has moved
+                    Instantiate(platformPrefab, currentMousePosition, Quaternion.identity);
+                    // update the previous mouse position
+                    previousMousePosition = currentMousePosition;
+
+                    // update draw counter
+                    platCounter--;
+                    platCount.text = platCounter.ToString() + "/" + initialPlatCount.ToString();
+                }
+            }
+        }
+
+        // for deactivator
+        if (deactActive)
+        {
+            border1.gameObject.SetActive(false);
+            border2.gameObject.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0) && deactCounter > 0 && !pauseMenu.gameObject.activeSelf && !endScreen.gameObject.activeSelf)
+            {
+                Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                currentMousePosition.z = 0f;
+
+                if (currentMousePosition != previousMousePosition && !RectTransformUtility.RectangleContainsScreenPoint(pauseBtn, currentMousePosition))
+                {
+                    Instantiate(deactPrefab, currentMousePosition, Quaternion.identity);
+                    previousMousePosition = currentMousePosition;
+
+                    deactCounter--;
+                    deactCount.text = deactCounter.ToString() + "/" + initialDeactCount.ToString();
+                }
+            }
+        }
+    }
+}
