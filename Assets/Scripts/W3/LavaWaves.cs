4944eab15128f0ea376ea6b95fd06b2fca6c5640
peenapple <justinye787@gmail.com>
2024-01-15 22:16:11 -0500

    lava waves wavin n shit

---
+++ Assets/Scripts/W3/LavaWaves.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaWaves : MonoBehaviour
+{
+    [SerializeField] float tweenTime = 120f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.moveLocalX(transform.gameObject, -140, tweenTime);
+    }
+}
